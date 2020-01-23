2fab9cb07f90b9802896b208eccb9c4e2e4b761c
Hubert Rządziński <h.rzadzinski@gmail.com>
2020-01-23 17:50:27 +0100

    Changed .js code to Airbnb Style

--- src/Components/TodoItem/TodoItem.js
+++ src/Components/TodoItem/TodoItem.js
@@ -1,32 +1,32 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class TodoItem extends Component {
-    render() {
-        const {id, title, description, completed} = this.props.todo;
-        const todoContent = (
-            <>
-                <h3>{title}</h3>
-                <p>{description}</p>
-                <button onClick={this.props.deleteTodo.bind(id)}>X</button>
-            </>
-        );
+  render() {
+    const { id, title, description, completed } = this.props.todo;
+    const todoContent = (
+      <>
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <button onClick={this.props.deleteTodo.bind(id)}>X</button>
+      </>
+    );
 
-        if (completed) {
-            return (
-                <li>
-                    <s>
-                        {todoContent}
-                    </s>
-                </li>
-            )
-        } else {
-            return (
-                <li>
-                    {todoContent}
-                </li>
-            )
-        }
+    if (completed) {
+      return (
+        <li>
+          <s>
+            {todoContent}
+          </s>
+        </li>
+      )
+    } else {
+      return (
+        <li>
+          {todoContent}
+        </li>
+      )
     }
+  }
 }
 
 export default TodoItem;
